a73a6283d4c6c281ac31f66fbccedbdcdca46700
Nilima-git <60936453+Nilima-git@users.noreply.github.com>
2020-12-31 13:16:17 +0530

    initial

---
+++ First_program.java
@@ -0,0 +1,10 @@
+public class First_program
+{
+	public static void main(String args[])
+	{
+	
+	System.out.println("Welcome to JAVA Programming");	
+	
+	}//End of the main method
+
+}//End of the class
